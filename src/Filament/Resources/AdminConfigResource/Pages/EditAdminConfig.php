d9f99a97a5cb461d6dfea88ad75c0d097ee01238
Patrick <privat@skyraptor.eu>
2022-06-12 03:39:14 +0200

    Add view button to admin configs

--- src/Filament/Resources/AdminConfigResource/Pages/EditAdminConfig.php
+++ src/Filament/Resources/AdminConfigResource/Pages/EditAdminConfig.php
@@ -3,9 +3,21 @@
 namespace SquadMS\AdminConfig\Filament\Resources\AdminConfigResource\Pages;
 
 use SquadMS\AdminConfig\Filament\Resources\AdminConfigResource;
+use Filament\Pages\Actions\Action;
 use Filament\Resources\Pages\EditRecord;
 
 class EditAdminConfig extends EditRecord
 {
     protected static string $resource = AdminConfigResource::class;
+ 
+    protected function getActions(): array
+    {
+        return array_merge(parent::getActions(), [
+            Action::make('settings')
+                ->label('View')
+                ->url(route('remoteAdmin', [
+                    'adminconfig' => $this->record
+                ])),
+        ]);
+    }
 }
