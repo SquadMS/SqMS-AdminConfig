38dcd64c8fe250034cc19f181c86f25c143896b0
Patrick <privat@skyraptor.eu>
2022-07-02 10:24:05 +0200

    Refactor: Make use of new SquadMSModuleProvider

--- routes/api.php
+++ routes/api.php
@@ -3,5 +3,10 @@
 use Illuminate\Support\Facades\Route;
 use SquadMS\AdminConfig\Http\Controllers\API\AdminConfigController;
 
-/* Remote admin config */
+Route::group([
+    'prefix'     => 'api',
+    'middleware' => 'api',
+], function () {
+    /* Remote admin config */
 Route::get('admin-cfg/{adminconfig}', [AdminConfigController::class, 'remoteAdmin'])->name('remoteAdmin');
+});
--- src/AdminConfigServiceProvider.php
+++ src/AdminConfigServiceProvider.php
@@ -2,32 +2,65 @@
 
 namespace SquadMS\AdminConfig;
 
-use Illuminate\Support\ServiceProvider;
-use SquadMS\AdminConfig\Providers\EventServiceProvider;
+use Illuminate\Support\Facades\Config;
+use Illuminate\Support\Facades\Validator;
+use Spatie\LaravelPackageTools\Package;
+use SquadMS\AdminConfig\Events\Internal\AdminConfig\AdminConfigSaving;
+use SquadMS\AdminConfig\Events\Internal\ServerGroup\ServerGroupRemovedPermission;
+use SquadMS\AdminConfig\Filament\Resources\AdminConfigResource;
+use SquadMS\AdminConfig\Filament\Resources\ServerGroupResource;
+use SquadMS\AdminConfig\Filament\Resources\ServerPermissionResource;
+use SquadMS\AdminConfig\Listeners\AdminConfig\ClearUsersMainGroupCache;
+use SquadMS\AdminConfig\Listeners\ServerGroup\ClearUsersReservedCache;
+use SquadMS\Foundation\Contracts\SquadMSModuleServiceProvider;
+use SquadMS\Foundation\Facades\SquadMSPermissions;
 
-class AdminConfigServiceProvider extends ServiceProvider
+class AdminConfigServiceProvider extends SquadMSModuleServiceProvider
 {
+    public static string $name = 'sqms-adminconfig';
+
+    protected array $resources = [
+        AdminConfigResource::class,
+        ServerGroupResource::class,
+        ServerPermissionResource::class,
+    ];
+
+    protected $listen = [
+        AdminConfigSaving::class => [
+            ClearUsersMainGroupCache::class,
+        ],
+        ServerGroupRemovedPermission::class => [
+            ClearUsersReservedCache::class,
+        ],
+    ];
+
+    public function configureModule(Package $package): void
+    {
+        $package->hasAssets()
+                ->hasConfigFile()
+                ->hasRoutes(['api']);
+    }
+
     /**
      * Register any application services.
      *
      * @return void
      */
-    public function register()
+    public function registeringModule(): void
     {
-        //
+        
     }
 
-    /**
-     * Bootstrap any application services.
-     *
-     * @return void
-     */
-    public function boot()
+    public function bootedModule(): void
     {
-        /* Configuration */
-        $this->mergeConfigFrom(__DIR__.'/../config/sqms-adminconfig.php', 'sqms-adminconfig');
-        
-        /* Load Migrations */
-        $this->loadMigrationsFrom(__DIR__ . '/../database/migrations');
+        /* Validates a HEX color code (like from color inputs) */
+        Validator::extend('color', function ($attribute, $value, $parameters, $validator) {
+            return preg_match('/^#[a-f0-9]{6}$/i', $value) === 1;
+        });
+
+        /* Permissions */
+        foreach (Config::get('sqms-adminconfig.permissions.definitions', []) as $definition => $displayName) {
+            SquadMSPermissions::define(Config::get('sqms-adminconfig.permissions.module'), $definition, $displayName);
+        }
     }
 }
